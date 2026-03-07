33ac48521b0037e22ee466e6a15a84c1f77bd5da
frog-wrangler <232842683+frog-wrangler@users.noreply.github.com>
2026-03-06 22:17:57 -0500

    style: Changed Alpha menu to be single-layered

--- lua/plugins/UI/alpha.lua
+++ lua/plugins/UI/alpha.lua
@@ -4,40 +4,19 @@ return {
         "nvim-tree/nvim-web-devicons",
         "nvim-lua/plenary.nvim",
     },
-    opts = function ()
+    opts = function()
         local dashboard = require("alpha.themes.dashboard")
 
         dashboard.section.header.val = require("data.banners")["random"]
 
-        local main_buttons = {
-            dashboard.button("o", "  > Open", ":lua Toggle_alpha_open_menu()<CR>"),
-            dashboard.button("s", "  > Search keymaps", ":Telescope keymaps<CR>"),
-            dashboard.button("l", "  > Lazy", ":Lazy<CR>"),
-            dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
-        }
-
-        local open_buttons = {
+        dashboard.section.buttons.val = {
             dashboard.button("o", "  > Open code", ":cd ~/sourceCode/ | :e .<CR>"),
+            dashboard.button("c", "  > Open config", ":cd " .. vim.fn.stdpath("config") .. " | :e .<CR>"),
             dashboard.button("q", "  > Open quickshell", ":cd ~/.config/quickshell/ | :e .<CR>"),
             dashboard.button("n", "  > Open notes", ":cd ~/notes/ | :e .<CR>"),
-            dashboard.button("c", "  > Open config", ":cd " .. vim.fn.stdpath("config") .. " | :e .<CR>"),
-            dashboard.button("e", "  > Exit", ":lua Toggle_alpha_open_menu()<CR>"),
+            dashboard.button("l", "  > Lazy", ":Lazy<CR>"),
+            dashboard.button("e", "  > Exit NVIM", ":qa<CR>"),
         }
-
-        local use_main = true
-
-        function Toggle_alpha_open_menu()
-            if use_main then
-                dashboard.section.buttons.val = open_buttons
-            else
-                dashboard.section.buttons.val = main_buttons
-            end
-            use_main = not use_main
-            vim.cmd("Alpha")
-            vim.cmd("Alpha")
-        end
-
-        dashboard.section.buttons.val = main_buttons
 
         local quote = require("data.quotes")
         dashboard.section.footer.val = quote()
